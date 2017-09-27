8fc79fd2f69aa89c9bea27103b23aa65049171cf
ninjakx <samurai.kittycat@gmail.com>
2017-09-27 20:50:47 +0530

    Delete test.py

--- InstagramCard/test.py
+++
@@ -1,9 +0,0 @@
-from get_data import *
-username = 'praggyasingh'
-import time
-
-url,image_link,link,name,detail,posts,followers,following=userprofile(username)
-profile_url= 'http://instagram.com/'+str(username)+'/'
-
-#print(c.link)
-
